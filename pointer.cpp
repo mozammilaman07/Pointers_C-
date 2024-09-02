d8d04655dd2d2ae5f79ac15cfb20dcf9898dbca3
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-02 23:45:42 +0530

    printed the size of char ptr

--- pointer.cpp
+++ pointer.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 int main()
 {
-    int a = 10;
-    cout << a << endl;
-    cout << &a << endl;
+    // int a = 10;
+    // cout << a << endl;
+    // cout << &a << endl;
 
-    // creation of pointer
-    int *ptr = &a;
+    // // creation of pointer
+    // int *ptr = &a;
 
-    // accessing value of ptr(address)
-    cout << ptr << endl;
-    // printing the value of a by ptr
-    cout << *ptr << endl;
-    cout << &ptr << endl;
+    // // accessing value of ptr(address)
+    // cout << ptr << endl;
+    // // printing the value of a by ptr
+    // cout << *ptr << endl;
+    // cout << &ptr << endl;
 
-    cout << "size of ptr : " << sizeof(ptr) << endl;
+    // cout << "size of ptr : " << sizeof(ptr) << endl;
+
+    char naam = 'k';
+    char *nptr = &naam;
+    cout << sizeof(nptr) << endl;
 }
